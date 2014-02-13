66c4120b9e30fb52791309b895d050170864329f
Leon <lk@billypds.com>
2014-02-13 12:07:55 +0100

    minor tweak

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 // the index.php in the root always displays all available webpipe-urls
 
 $rooturl  = str_replace("//","/", "http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['REQUEST_URI'])."/" );
+// trailing space to not lose POST-data because of apache 301 redirect
+$trail    = strstr( $_SERVER['HTTP_HOST'], "appspot" ) ? "" : "/";
 $webpipes = array();
 $files = scandir( dirname(__FILE__) );
 foreach ($files as $file) {
     if ($file === '.' or $file === '..' or $file === '.git' ) continue;
-    if (is_dir($file)) $webpipes[]= $rooturl.$file."/"; // trailing space to not lose POST-data because of 301 redirect
+    if (is_dir($file)) $webpipes[]= $rooturl.$file.$trail;
 }
 printf("%s", implode("\n",$webpipes) );
 
