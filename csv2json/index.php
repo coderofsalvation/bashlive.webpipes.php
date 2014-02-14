4c92212c86dba31d7284046bb658e8ae5d6916f0
Leon <lk@billypds.com>
2014-02-14 12:35:46 +0100

    minor fixes

--- csv2json/index.php
+++ csv2json/index.php
@@ -8,7 +8,6 @@ die( $_SERVER['REQUEST_METHOD'] == "POST" ? servePost() : serveOptions() );
 function process( $input ){
   if( !$input ) return serveOptions();
   $delimiter = isset($_GET['delimiter']) ? $_GET['delimiter'] : ",";
-  return print_r($_GET);
   $arr = array();
   $lines = explode("\n", $input );
   foreach( $lines as $line ) $arr[] = str_getcsv( $line, $delimiter );
