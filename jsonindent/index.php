8ca1d219767e6dbfe057dacb15d9c84d2da7aee1
Coder of Salvation <info@leon.vankammen.eu>
2014-04-06 02:26:09 +0200

    added jsonindent

---
+++ jsonindent/index.php
@@ -0,0 +1,86 @@
+<?
+
+include_once("../webpipe.php");
+
+die( $_SERVER['REQUEST_METHOD'] == "POST" ? servePost() : serveOptions() );
+// every webpipe starts in the process function
+
+function process( $input ){
+  //if( $_SERVER['CONTENT_TYPE'] != "application/json" ) $input = json_decode($input);
+  if( !$input ) return "invalid input";
+  return json_format($input);
+}
+
+function json_format($json)
+{
+    $tab = "  ";
+    $new_json = "";
+    $indent_level = 0;
+    $in_string = false;
+
+    $len = strlen($json);
+
+    for($c = 0; $c < $len; $c++)
+    {
+        $char = $json[$c];
+        switch($char)
+        {
+            case '{':
+            case '[':
+                if(!$in_string)
+                {
+                    $new_json .= $char . "\n" . str_repeat($tab, $indent_level+1);
+                    $indent_level++;
+                }
+                else
+                {
+                    $new_json .= $char;
+                }
+                break;
+            case '}':
+            case ']':
+                if(!$in_string)
+                {
+                    $indent_level--;
+                    $new_json .= "\n" . str_repeat($tab, $indent_level) . $char;
+                }
+                else
+                {
+                    $new_json .= $char;
+                }
+                break;
+            case ',':
+                if(!$in_string)
+                {
+                    $new_json .= ",\n" . str_repeat($tab, $indent_level);
+                }
+                else
+                {
+                    $new_json .= $char;
+                }
+                break;
+            case ':':
+                if(!$in_string)
+                {
+                    $new_json .= ": ";
+                }
+                else
+                {
+                    $new_json .= $char;
+                }
+                break;
+            case '"':
+                if($c > 0 && $json[$c-1] != '\\')
+                {
+                    $in_string = !$in_string;
+                }
+            default:
+                $new_json .= $char;
+                break;                   
+        }
+    }
+
+    return $new_json;
+}
+
+?>
